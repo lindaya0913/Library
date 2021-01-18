d3076b74f7f2f58a19d2ebbf46b3dd2aeed21ac6
lindaya0913 <you@example.com>
2021-01-18 17:18:18 +0800

    table done

--- controller.js
+++ controller.js
@@ -1,46 +1,183 @@
 window.onload = function () {
-    var json="20210106.txt";/*json文件url，本地的就寫本地的位置，如果是服務器的就寫服務器的路徑*/
+    var txt = "20210106.txt";/*txt文件url，本地的就寫本地的位置，如果是服務器的就寫服務器的路徑*/
     var request = new XMLHttpRequest();
-    request.open("get", json);/*設置請求方法與路徑*/
+    request.open("get", txt);/*設置請求方法與路徑*/
     request.send(null);/*不發送數據到服務器*/
     request.onload = function () {/*XHR對象獲取到返回信息後執行*/
         if (request.status == 200) {/*返回狀態為200，即為數據獲取成功*/
-            // var a = JSON.parse(request.responseText);
-            // console.log(a);
             var splitResponse = request.responseText.split("\n");
-            // var splitResponse = JSON.stringify(splitResponse)
-            console.log(splitResponse);
-            console.log(splitResponse[1]);
-            var splitResponse2 = [];
-            var i = 0;
-            // splitResponse2 += splitResponse.indexOf("~")
-            // console.log(splitResponse2);
-            for(index = 1; index < splitResponse.length; index+2){
-                splitResponse2[i] += splitResponse[index];
-                // splitResponse2 = splitResponse.indexOf("~")
-                i+=1
-            } 
-            document.getElementById("status").innerHTML = splitResponse; 
+            var times = splitResponse.filter((item, index) => index % 2 == 0);
+            var year = splitResponse.filter((item, index) => index % 2 != 0);
+            var data = [];
+            var year13 = [], year17 = [], year18 = [], year19 = [], year20 = [], year21 = [];
+            var a = 0, e = 0, f = 0, g = 0, h = 0, i = 0;
+            times.shift()/*去掉空格*/
+            year = year.map(x => x.slice(1,11))/*選取YYYY/MM/DD*/
+
+            for (var n = 0; n < times.length; n ++) {
+                data[n]={"year":year[n], "times":times[n]};
+            }
+
+            for (var m = 0; m < data.length; m ++) {
+                if(data[m].year.match('2013')){
+                    year13[a] = data[m]
+                    a += 1
+                }
+                if(data[m].year.match('2017')){
+                    year17[e] = data[m]
+                    e += 1
+                }
+                if(data[m].year.match('2018')){
+                    year18[f] = data[m]
+                    f += 1
+                }
+                if(data[m].year.match('2019')){
+                    year19[g] = data[m]
+                    g += 1
+                }
+                if(data[m].year.match('2020')){
+                    year20[h] = data[m]
+                    h += 1
+                }
+                if(data[m].year.match('2021')){
+                    year21[i] = data[m]
+                    i += 1
+                }
+            }
+
+            var select = document.getElementById("select").value
+            var trStr1 = '', trStr2 = '', trStr3 = '', trStr4 = '', trStr5 = '', trStr6 = '',
+                trStr7 = '', trStr8 = '', trStr9 = '', trStr10 = '', trStr11 = '', trStr12 = ''
+            var ll = 0
+            console.log(select);
+            //year2020
+            if (select = "2020"){
+                for (var l = 0; l < year20.length; l ++) {//迴圈遍歷出json物件中的每一個資料並顯示在對應的td中
+                    if(year20[l].year.match('/01/')){
+                        trStr1 += '<tr>';  
+                        trStr1 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr1 += '<td>' + year20[l].times + '</td>';
+                        trStr1 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/02/')){
+                        trStr2 += '<tr>';  
+                        trStr2 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr2 += '<td>' + year20[l].times + '</td>';
+                        trStr2 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/03/')){
+                        trStr3 += '<tr>';  
+                        trStr3 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr3 += '<td>' + year20[l].times + '</td>';
+                        trStr3 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/04/')){
+                        trStr4 += '<tr>';  
+                        trStr4 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr4 += '<td>' + year20[l].times + '</td>';
+                        trStr4 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/05/')){
+                        trStr5 += '<tr>';  
+                        trStr5 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr5 += '<td>' + year20[l].times + '</td>';
+                        trStr5 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/06/')){
+                        trStr6 += '<tr>';  
+                        trStr6 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr6 += '<td>' + year20[l].times + '</td>';
+                        trStr6 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/07/')){
+                        trStr7 += '<tr>';  
+                        trStr7 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr7 += '<td>' + year20[l].times + '</td>';
+                        trStr7 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/08/')){
+                        trStr8 += '<tr>';  
+                        trStr8 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr8 += '<td>' + year20[l].times + '</td>';
+                        trStr8 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/09/')){
+                        trStr9 += '<tr>';  
+                        trStr9 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr9 += '<td>' + year20[l].times + '</td>';
+                        trStr9 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/10/')){
+                        trStr10 += '<tr>';  
+                        trStr10 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr10 += '<td>' + year20[l].times + '</td>';
+                        trStr10 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/11/')){
+                        trStr11 += '<tr>';  
+                        trStr11 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr11 += '<td>' + year20[l].times + '</td>';
+                        trStr11 += '</tr>';  
+                    }
+                    if(year20[l].year.match('/12/')){
+                        trStr12 += '<tr>';  
+                        trStr12 += '<td>' + year20[l].year + '</td>';//資料表的主鍵值
+                        trStr12 += '<td>' + year20[l].times + '</td>';
+                        trStr12 += '</tr>';  
+                    }
+                } 
+            }
+            // console.log("select", select);
+            // document.getElementById("status").innerHTML = splitResponse; 
 
             //table
+            $('#tbody_id1').append(trStr1);
             $('#table_id1').DataTable({
-                "order": [ 4, 'asc' ],
-                rowCallback: function(row, data, index){
-                    //suspicious
-                    // if(data[4] == 'true'){
-                    //     $(row).find('td:eq(4)').html('<i class="fas fa-check"></i>').css('color', 'Green');
-                    // }else if (data[4] == 'false'){
-                    //     $(row).find('td:eq(4)').html('<i class="fas fa-skull-crossbones"></i>').css({'color':'Red','font-size':'20px'});
-                    // }
-                    // //domain_name
-                    // if(data[2] == 'null'){
-                    //     $(row).find('td:eq(2)').html('-');
-                    // }
-                    // //city
-                    // if(data[6] == 'null'){
-                    //     $(row).find('td:eq(6)').html('-');
-                    // }
-                },
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id2').append(trStr2);
+            $('#table_id2').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id3').append(trStr3);
+            $('#table_id3').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id4').append(trStr4);
+            $('#table_id4').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id5').append(trStr5);
+            $('#table_id5').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id6').append(trStr6);
+            $('#table_id6').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id7').append(trStr7);
+            $('#table_id7').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id8').append(trStr8);
+            $('#table_id8').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id9').append(trStr9);
+            $('#table_id9').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id10').append(trStr10);
+            $('#table_id10').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id11').append(trStr11);
+            $('#table_id11').DataTable({
+                "order": [ 0, 'asc' ],
+            });
+            $('#tbody_id12').append(trStr12);
+            $('#table_id12').DataTable({
+                "order": [ 0, 'asc' ],
             });
 
             var ctx = document.getElementById('myChart').getContext('2d');
